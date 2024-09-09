33c4603665d754f167f5d19fbd25fea00de7d357
ali-ledezma <a.22308051281229@cbtis128.edu.mx>
2024-09-09 10:12:54 -0600

    Add files via upload
    mi lista y tuplas

---
+++ mi_lista.py
@@ -0,0 +1,18 @@
+
+print("Ejemplo de listas")
+arcoiris=["verde ", "azul", "morado"]
+print(arcoiris)
+longitud=len(arcoiris)
+print(longitud)
+print("Elementos que tiene la lista arcoiris ", longitud)
+print(f"Elementos que tiene la lista arcoiris v2 {longitud}")
+print("accediendo a un elemento de la linea")
+print(arcoiris[1])  
+print(f"elemento en la posicion cero es {arcoiris[0]}")  
+print(f"elemento en la posicion cero es ",{arcoiris[0]})   
+print("agregar a un elemento a la lista")
+arcoiris.append("naranja")
+print(arcoiris)
+print("listar los elementos de la lista ciclo for")
+for ledezma in arcoiris:
+    print(ledezma)
---
+++ mi_tuplas.py
@@ -0,0 +1,8 @@
+print("Ejemplo de tupas")
+canciones=("Te amo", "El noanoa", "amor eterno")
+print(canciones)
+y = list(canciones)
+print(canciones)
+y[1] = "la puerta negra"
+x = tuple(y)
+print(x) 
